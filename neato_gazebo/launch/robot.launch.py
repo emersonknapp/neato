c56f0aa28d60aed569ab200666f46d09ff07d8cd
Emerson Knapp <emerson.b.knapp@gmail.com>
2019-12-02 01:52:35 -0800

    Launching gazebo with front end

--- neato_gazebo/launch/robot.launch.py
+++ neato_gazebo/launch/robot.launch.py
@@ -41,7 +41,7 @@ def generate_launch_description():
     return LaunchDescription([
         launch.actions.ExecuteProcess(
             cmd=[
-                'gzserver', '--verbose',
+                'gazebo', '--verbose',
                 '-s', 'libgazebo_ros_init.so',
                 '-s', 'libgazebo_ros_factory.so',
             ],
