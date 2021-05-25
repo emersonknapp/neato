3ee0dce933020624f8d3b2cd9e4ab1df8ff84862
Emerson Knapp <eknapp@amazon.com>
2021-05-25 14:43:31 -0700

    Add optional joint_state_publisher to description.launch.py
    Signed-off-by: Emerson Knapp <eknapp@amazon.com>

--- neato_description/launch/description.launch.py
+++ neato_description/launch/description.launch.py
@@ -4,6 +4,7 @@ import tempfile
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 import xacro
@@ -25,21 +26,23 @@ def generate_launch_description():
 
     return LaunchDescription([
         DeclareLaunchArgument('use_sim_time', default_value='false'),
+        DeclareLaunchArgument('joint_states', default_value='false'),
         Node(
             package='robot_state_publisher',
-            node_executable='robot_state_publisher',
-            node_name='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
             output='screen',
             parameters=[{
                 'publish_frequency': 5.0,
                 'use_sim_time': LaunchConfiguration('use_sim_time')}],
             arguments=[urdf_file],
         ),
-        # Node(
-        #     package='joint_state_publisher',
-        #     node_executable='joint_state_publisher',
-        #     name='joint_state_publisher',
-        #     output='screen',
-        #     arguments=[urdf_file.name],
-        # ),
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen',
+            arguments=[urdf_file],
+            condition=IfCondition(LaunchConfiguration('joint_states')),
+        ),
     ])
